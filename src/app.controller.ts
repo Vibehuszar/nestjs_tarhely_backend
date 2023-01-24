3435e821af8b45f465d1385b6240e5f6b67a912f
Dani <hostnameadmin@protonmail.com>
2023-01-24 11:27:47 +0100

    update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -31,7 +31,6 @@ export class AppController {
   }
 
   @Post('/api/tarhely')
-  @Redirect()
   async insertTarhely(@Body() tarhelydata: TarhelyDataDto) {
     await db.execute('INSERT INTO tarhelycsomagok (nev, meret, ar) VALUES (?, ?, ?)',[
       tarhelydata.nev, tarhelydata.meret, tarhelydata.ar,
